dce741208a7cf75206797cb40fe7ffd996008712
li <2652984264@qq.com>
2023-07-31 09:44:58 +0800

    10

--- src/main/java/com/lcx/Base64Util.java
+++ src/main/java/com/lcx/Base64Util.java
@@ -103,26 +103,29 @@ public class Base64Util {
 //        System.out.println(Base64Util.imageUrlToBase64("https://img-blog.csdnimg.cn/20210105221008901.png"));
 //        d(Base64Util.imageUrlToBase64("https://img-blog.csdnimg.cn/20210105221008901.png"));
         //获取输入流
-        URL url = new URL("https://img-blog.csdnimg.cn/20210105221008901.png");
+        URL url = new URL("https://res.sxspv.com:4433/resources/images/data_screen/6250b23c54fdd6ab18dd7e50aa79566c.gif");
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
         InputStream bis = httpURLConnection.getInputStream();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
         //获取输出流
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] bytes = new byte[1024];
         int len;
         while ((len = bis.read(bytes)) != -1) {
             baos.write(bytes, 0, len);
-
         }
+
         //转成字符串base64
         Base64.Encoder encoder = Base64.getEncoder();
         String s = encoder.encodeToString(baos.toByteArray());
         System.out.println(s);
+
         //解码成字节码数组
         Base64.Decoder decoder = Base64.getDecoder();
         byte[] decode = decoder.decode(s);
+
         //输出流输出
-        BufferedOutputStream bos = new BufferedOutputStream(Files.newOutputStream(Paths.get("a.png")));
+        BufferedOutputStream bos = new BufferedOutputStream(Files.newOutputStream(Paths.get("a.gif")));
         bos.write(decode);
 
         bos.close();
